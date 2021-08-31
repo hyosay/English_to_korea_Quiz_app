c552fd224b34594fcd75c5339ef2693d0b70a70a
Hyoseong Jeon <gytjdwjs@cu.ac.kr>
2021-08-31 23:22:22 +0900

    21.08.31

--- login.py
+++ login.py
@@ -1,5 +1,6 @@
 import pymysql
 import numpy as np
+import pandas as pd
 
 #localhost mysql 접속
 conn = pymysql.connect(
@@ -8,82 +9,125 @@ conn = pymysql.connect(
     host= 'localhost',
     charset='utf8'
 )
+# pandas를 통해 데이터를 시각화
+# numpy를 통해서 데이터를 전제
+# 변수 설명
+'''회원가입 변수
+user_rows: root에 있는 '''
+
 
 #커서 생성
 cursor = conn.cursor()
-#user 만들기 회원가입
-#회원가입 스키마까지 생성
-cursor.execute('use mysql')
-cursor.execute('select user from user')
-user_rows = cursor.fetchall()
-user_rows = np.array(user_rows).flatten().tolist()
-print(user_rows)
-count = 5
-while count:
-    create_user = input('사용할 아이디를 입력해주세요. : ')
-    if create_user in user_rows:
-        print('이미 존재하는 아이디 입니다.',end= ' ')
-        if count == 1:
-            print('회원가입을 처음부터 다시 진행해주십시요.')
-            break
-        else:
-            print('아이디를 다시 입력해주세요({}/5)'.format(6 - count))
-            count -= 1
-    else:
-        for passwd_count in range(1, 6):
-            create_passwd = input('사용할 비밀번호를 입력해주세요.(영문숫자포함8자리)')
-            if create_passwd.isalnum() and len(create_passwd) >= 8:
-                print('아이디가 생성되었습니다.')
-                cursor.execute("use mysql")
-                cursor.execute("create user {0}@localhost identified by '{1}'".format(create_user, create_passwd))
-                cursor.execute("create schema {}".format(create_user))
-                cursor.execute("grant all privileges on {0}.* to {1}@localhost".format(create_user, create_user))
-                cursor.execute("flush privileges")
-                count = 0
+def sign_up():
+    #user 만들기 회원가입
+    #회원가입 스키마까지 생성
+    cursor.execute('use mysql')
+    cursor.execute('select user from user')
+    user_rows = cursor.fetchall()
+    user_rows = np.array(user_rows).flatten().tolist()
+    print(user_rows)
+    count = 5
+    while count:
+        create_name = input('이름을 입력해주세요. : ')
+        create_id = input('사용할 아이디를 입력해주세요. : ')
+        if create_id in user_rows:
+            print('이미 존재하는 아이디 입니다.',end= ' ')
+            if count == 1:
+                print('회원가입을 처음부터 다시 진행해주십시요.')
                 break
             else:
-                if passwd_count == 5:
-                    print('회원가입을 처음부터 다시 진행해주십시요.')
+                print('아이디를 다시 입력해주세요({}/5)'.format(6 - count))
+                count -= 1
+        else:
+            for passwd_count in range(1, 6):
+                create_passwd = input('사용할 비밀번호를 입력해주세요.(영문숫자포함8자리)')
+                if create_passwd.isalnum() and len(create_passwd) >= 8:
+                    print('아이디가 생성되었습니다.')
+                    # mysql에 유저 아이디, 비밀번호 등록
+                    cursor.execute("use mysql")
+                    cursor.execute("create user {0}@localhost identified by '{1}'".format(create_id, create_passwd))
+                    # 아이디와 동일한 DB생성 및 본인 DB에 대한 권한부여
+                    cursor.execute("create schema {}".format(create_id))
+                    cursor.execute("grant all privileges on {0}.* to {1}@localhost".format(create_id, create_id))
+                    # 운영자 mysql에 유저 정보입력
+                    cursor.execute("use user_info")
+                    cursor.execute("INSERT INTO user_info (id, name, passwd) VALUES (\'{0}\', \'{1}\', \'{2}\')".format(create_id, create_name, create_passwd))
+
+                    # 본인 이름의 DB에 단어장 생성
+                    cursor.execute('use {0}'.format(create_id))
+                    table = '''CREATE TABLE word (
+                    영어 VARCHAR(30) NOT NULL,
+                    한국어 VARCHAR(30) NOT NULL
+                    )'''
+                    cursor.execute(table)
+                    # 저장
+                    cursor.execute("flush privileges")
                     count = 0
+                    break
                 else:
-                    print('비밀번호 조건에 일치하지 않습니다. 사용할 비밀버호를 다시 입력해주세요.({}/5)'.format(passwd_count))
+                    if passwd_count == 5:
+                        print('회원가입을 처음부터 다시 진행해주십시요.')
+                        count = 0
+                    else:
+                        print('비밀번호 조건에 일치하지 않습니다. 사용할 비밀버호를 다시 입력해주세요.({}/5)'.format(passwd_count))
 
 
+def login():
+    #user mysql 접속
+    id = input('아이디를 입력해주세요 : ')
+    passwd = input('비밀번호를 입력해주세요 : ')
+
+    #1차보안
+    cursor.execute('use mysql')
+    cursor.execute('SELECT user FROM user')
+    user_rows = np.array(cursor.fetchall()).flatten().tolist()
+
+    cursor.execute('use user_info')
+    cursor.execute('SELECT id, passwd from user_info')
+    user_info_rows = cursor.fetchall()
 
 
+    #접속
+    if (id, passwd) in user_info_rows and id in user_rows:
+        print('로그인이 확인 되었습니다.')
+
+        user = pymysql.connect(
+                user='{}'.format(id),
+                passwd='{}'.format(passwd),
+                host= 'localhost',
+                charset='utf8',
+                db= '{}'.format(id)
+            )
+        user_cursor = user.cursor()
 
 
+        # 데이터 삽입하기
+        while 1:
+            print('옵션을 선택해주세요. 1. 단어장 확인 \t 2. 데이터 추가하기, 3. 종료')
+            k = int(input())
+            if k == 1:
+                user_cursor.execute('select * from word')
+                word_list = user_cursor.fetchall()
+                word_eng = np.array(word_list).flatten().tolist()[0::2]
+                word_kor = np.array(word_list).flatten().tolist()[1::2]
 
-'''
-#user mysql 접속
-_user = input('아이디를 입력해주세요 : ')
-passwd = input('비밀번호를 입력해주세요 : ')
-print(_user)
-user = pymysql.connect(
-    user='{}'.format(_user),
-    passwd='{}'.format(passwd),
-    host= 'localhost',
-    charset='utf8'
-)
-user_cursor = user.cursor()
-
-cursor.execute('use mysql')
-cursor.execute('SELECT host, user FROM user')
-user_rows = cursor.fetchall()
-
-for i, j in user_rows:
-    if j == _user:
-        print('확인')
-        
-        user_cursor.execute('USE {}'.format(_user))
-user_cursor.execute('show databases;')
-rows = user_cursor.fetchall()
-
-print(rows)'''
+                result = pd.DataFrame({'eng': word_eng,
+                                       'kor': word_kor})
+                print(result)
 
 
+            elif k == 2:
+                eng = input('영어를 입력하세요 : ')
+                kor = input('한글을 입력하세요 : ')
+                user_cursor.execute('INSERT INTO word (영어, 한국어) VALUES (\'{0}\', \'{1}\')'.format(eng, kor))
 
-
+            elif k == 3:
+                print('프로그램을 종료합니다.')
+                user.commit()
+                user.close()
+                break
+    else:
+        print('no')
 
 
 
@@ -103,7 +147,7 @@ def mk_user():
 login_count = 0
 
 #변경예정
-def login():
+'''def login():
     global login_count
 
     login_key = input('이름을 입력해주세요. : ')
@@ -128,13 +172,13 @@ def login():
             print('보안을 위해 종료합니다.')
             return print('안녕히계세요')
         return login()
+'''
 
 
+login()
 conn.commit()
 
-'''user.commit()
 
-
-user.close()'''
 conn.close()
+
 
